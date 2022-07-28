04878651f90d5565d043a776dadabc8799daad1d
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-07-28 13:34:38 -0500

    Implement Policy

--- src/components/sections/Policy.jsx
+++ src/components/sections/Policy.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import TextBlock from '../blocks/TextBlock';
 
 const Policy = () => {
+  const headingStr = `Our Policies.`;
+  const bodyStr = `We work hard to serve all of our customers! To continue our goal to serve you, we have designed a set of policies to allow you to have the absolute greated shopping experience at La-Tiendita!`;
+
   return (
-    <div>Policy</div>
+    <section>
+      <p>Modal Animated Popup</p>
+      <TextBlock 
+        heading = {headingStr}
+        body = {bodyStr}
+      />
+    </section>
   );
 }
 
