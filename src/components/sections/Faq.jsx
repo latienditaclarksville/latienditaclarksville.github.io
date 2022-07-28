7ec2bb8ca32e0564f5a07107adc7e30c0ece2f02
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-07-28 13:34:13 -0500

    Implement Faq

--- src/components/sections/Faq.jsx
+++ src/components/sections/Faq.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import TextBlock from '../blocks/TextBlock';
 
 const Faq = () => {
+  const headerStr = `Some FAQs`;
+  const bodyStr = `Here are the most common question we get asked! If you don't see your question, feel free to send us an email at Francisco.2000@icloud.com or call us as (931)278-9303. We'd love to answer any questions you may have!`;
+
   return (
-    <div>Faq</div>
+    <section>
+      <TextBlock 
+        heading={headerStr}
+        body={bodyStr}
+      />
+      <p>Modal Animated Popup</p>
+    </section>
   );
 }
 
