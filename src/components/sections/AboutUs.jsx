0cf08bc87acc15eaff419a85595adb930bb69592
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-07-28 13:32:59 -0500

    Implement AboutUs

--- src/components/sections/AboutUs.jsx
+++ src/components/sections/AboutUs.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import TextBlock from '../blocks/TextBlock';
+import FamilySVG from '../../assets/illustrations/family.svg';
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <section className="flex flex-row">
+      <TextBlock 
+        heading='About Us'
+        body='Mother and Son.....'/>
+      <div>
+        <img src={FamilySVG} alt="A family eating dinner" />
+      </div>
+      <h1>Testing Tailwind</h1>
+    </section>
   );
 }
 
