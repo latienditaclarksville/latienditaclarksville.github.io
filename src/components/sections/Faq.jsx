dec469b4bf31abdcf62ef96c20b51bc99db5d11d
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-07-29 00:05:35 -0500

    Update FAQ layout

--- src/components/sections/Faq.jsx
+++ src/components/sections/Faq.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import TextBlock from '../blocks/TextBlock';
 
 const Faq = () => {
-  const headerStr = `Some FAQs`;
+  const headerStr = `FAQs`;
   const bodyStr = `Here are the most common question we get asked! If you don't see your question, feel free to send us an email at Francisco.2000@icloud.com or call us as (931)278-9303. We'd love to answer any questions you may have!`;
 
   return (
-    <section>
-      <TextBlock 
-        heading={headerStr}
-        body={bodyStr}
-      />
-      <p>Modal Animated Popup</p>
+    <section className="py-20">
+      <div className='flex 
+                      flex-row
+                      justify-between
+                      w-11/12
+                      mx-auto
+                      border'>
+        <TextBlock 
+          heading={headerStr}
+          body={bodyStr}
+        />
+        <p>Modal Animated Popup</p>
+      </div>
     </section>
   );
 }
