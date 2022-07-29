82134bafec14f059d7c73ff1cb74395ef7f3dc0c
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-07-29 00:04:50 -0500

    Implement TextBox component

--- src/components/blocks/TextBlock.jsx
+++ src/components/blocks/TextBlock.jsx
@@ -4,9 +4,12 @@ const TextBlock = (props) => {
   const { heading, body } = props;
 
   return (
-    <article className="flex flex-col">
-      <h2 className="font-bold">{heading}</h2>
-      <p className="font-light">{body}</p>
+    <article className="flex flex-col border w-6/12">
+      <h2 className="font-bold text-4xl py-3">{heading}</h2>
+      <p className="font-light 
+                    text-xl
+                    border
+                    ">{body}</p>
     </article>
   );
 }
